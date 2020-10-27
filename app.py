236090f71b33488062f49556898e2f09925f8daa
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2020-10-27 21:20:11 +0300

    page route,login and register html

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app=Flask(__name__,template_folder='templates')
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def signup():
+    return render_template('register.html')
+@app.route('/braintumordetect')
+def tumor():
+    return render_template('braintumordetect.html')
 #test
 """@app.route('/login', methods=['GET', 'POST'])
 def login():
