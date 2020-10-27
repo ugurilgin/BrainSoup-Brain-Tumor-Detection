86f31f865bd0d77ae2fe1bb2585fb923c4271e8d
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2020-10-27 18:39:57 +0300

    route for index.html and definition static files

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
-app=Flask(__name__)
+from flask import request
+from flask import render_template
+
+app=Flask(__name__,template_folder='templates')
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 #test
+"""@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()"""
